61d17c5b2b7c31f791e1e861569b7edad2c50bfc
danito2 <danielalejandroescalante@hotmail.com>
2023-03-24 02:36:58 -0300

    homework 03

---
+++ practice/controldeflujos.js
@@ -0,0 +1,23 @@
+function viajar(destino){
+    if (destino === "brasil") {
+        console.log("cuidado, con los monos");
+    } else if(destino === "argentina") {
+        console.log("el pais solo aguanta gracias a Messi");
+    } else {
+    console.log ("nos perdimos");
+}
+}
+
+viajar("Prueba") 
+
+
+function puedeManejar(edad) {
+    if(edad >= 18) {
+             console.log( true );
+    } 
+    console.log( false );
+  }
+  
+  puedeManejar(17);
+
+
---
+++ practice/funciones.js
@@ -0,0 +1,10 @@
+function sumaTres(x){
+    console.log(x + 3);
+}
+sumaTres(5);
+
+// primero
+
+function sumaTres(x){
+    return(x + 3);
+}
